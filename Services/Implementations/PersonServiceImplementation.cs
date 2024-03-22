a7273a76b66c6483a07457c22b568b6caf756cf7
VitorScallen77 <sscallen199@gmail.com>
2024-03-22 13:49:07 -0300

    Implementação de ID

---
+++ Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,67 @@
+﻿using RestWithASPNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RestWithASPNET.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+
+            return persons;
+        }
+
+        public Person FindByID(long id)
+        {
+            return new Person
+            {
+                Id = IncrementeAndGet(),
+                FirstName = "Vitor",
+                LastName = "Scallen",
+                Address = "São Paulo - SP",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+        
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementeAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person LastName" + i,
+                Address = "Some Address" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementeAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
--- obj/Debug/net5.0/RestWithASPNET.AssemblyInfo.cs
+++ obj/Debug/net5.0/RestWithASPNET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestWithASPNET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3945c3c4bd1f6e8546f9bda656e9e7005c06f2fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6b5bf195bfd54956544af32039b24b35ec6d23a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestWithASPNET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestWithASPNET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
